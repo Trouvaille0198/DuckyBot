a6d51d85e8abe9f184e3a940aba314030eab8db9
Trouvaille0198 <1375911011@qq.com>
2021-05-26 23:16:36 +0800

    update weather

--- src/plugins/weather/__init__.py
+++ src/plugins/weather/__init__.py
@@ -3,21 +3,33 @@ from nonebot import get_driver
 from .config import Config
 from nonebot.log import logger
 
-from nonebot import on_command
+from nonebot import on_command, on_regex
 from nonebot.rule import to_me
 from nonebot.typing import T_State
 from nonebot.adapters import Bot, Event
 
+import json
+
 global_config = get_driver().config
 config = Config(**global_config.dict())
 
+with open(r'src/assets/area.txt') as file_obj:
+    areas = file_obj.read().split()
 
-weather = on_command("天气", rule=to_me(), priority=5)
+weather = on_regex("(天气)|(天气如何)|(天气怎么样)", rule=to_me(), priority=5)
 
 
 @weather.handle()
 async def handle_first_receive(bot: Bot, event: Event, state: T_State):
-    args = str(event.get_message()).strip()  # 首次发送命令时跟随的参数，例：/天气 上海，则args为上海
+
+    msg = str(event.get_message())  # 首次发送命令时跟随的参数，例：/天气 上海，则args为上海
+    # await weather.send(cities[0]['cityName'])
+    for area in areas:
+        area = area[:-1] if (len(area) > 2) else area
+        if area in msg:
+            await weather.send(area)
+            break
+
     if args:
         state["city"] = args  # 如果用户发送了参数则直接赋值
 
